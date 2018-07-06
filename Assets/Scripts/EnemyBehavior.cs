fda1f9a5f7f3ca2f52a6ab79d474afbebb7ea2f7
rayarriaga <40896828+rayarriaga@users.noreply.github.com>
2018-07-06 12:43:54 -0500

    Initial commit

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public bool movesRight;
+    public float speed;
+    public float dspeed;
+    private Transform t;
+
+    // Use this for initialization
+    void Start ()
+    {
+        t = GetComponent<Transform>();
+        speed = 0.1f;
+        dspeed = -2.0f;
+        
+        movesRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = t.localPosition.x;
+        float y = t.localPosition.y;
+
+        if (x>= 11.5f)
+        {
+            movesRight = false;
+            t.localPosition += new Vector3(0.0f, dspeed);
+        }
+
+        if (x <= -11.5f)
+        {
+            movesRight = true;
+            t.localPosition += new Vector3(0.0f, dspeed);
+        }
+
+        if (movesRight == true)
+        {
+            t.localPosition += new Vector3(speed, 0.0f);
+        }
+        else
+        {
+            t.localPosition -= new Vector3(speed, 0.0f);
+        }
+
+        if (y == -4)
+        {
+            Debug.LogError("You lose!");
+            return;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float speed;
+    private Transform t;
+
+	// Use this for initialization
+	void Start ()
+    {
+        t = GetComponent<Transform>();
+        speed = 0.18f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = t.localPosition.x;
+        if (Input.GetKey(KeyCode.D))
+        {  
+            if (x<= 11.5f)
+            {
+                t.localPosition += new Vector3(speed, 0.0f);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (x >= -11.5f)
+            {
+                t.localPosition -= new Vector3(speed, 0.0f);
+            }
+        }
+    }
+}
